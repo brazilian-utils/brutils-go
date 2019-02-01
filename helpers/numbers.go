66be6204b0795461e5aba5935ef35c26aeeeb483
Rafael Acioly <aciolyr@gmail.com>
2019-02-01 15:17:38 -0200

    change OnlyNumbers method to remove everything that is not a number

--- helpers/numbers.go
+++ helpers/numbers.go
@@ -2,12 +2,10 @@ package helpers
 
 import (
 	"regexp"
-	"strings"
 )
 
 // OnlyNumbers removes all non
 // numeric characters from a string
 func OnlyNumbers(numbers string) string {
-	numericStr := regexp.MustCompilePOSIX("[0-9]+").FindAllString(numbers, -1)
-	return strings.Join(numericStr[:], "")
+	return regexp.MustCompile("[^0-9]").ReplaceAllString(numbers, "")
 }
